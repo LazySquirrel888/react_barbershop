ba835a999768372063b9651c39defc10acc49bb7
Tatiana <47958747+LazySquirrel888@users.noreply.github.com>
2019-06-06 08:59:59 +0300

    Updates

---
+++ precache-manifest.c96411dc7ab16b099c74d113000e4736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a16a645f86d1790dd27ee9d898d75a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81af6dd60feccce4a445",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "c5774ffaa29e2a521a3e",
+    "url": "/static/js/2.fc63af4f.chunk.js"
+  },
+  {
+    "revision": "81af6dd60feccce4a445",
+    "url": "/static/js/main.47e02404.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_barbershop/precache-manifest.87cd31207f82c45e5693a4ed12316416.js"
+  "/precache-manifest.c96411dc7ab16b099c74d113000e4736.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_barbershop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
