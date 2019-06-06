3e2c5b62274597a88da72b3c507195defb2769a9
Tatiana <47958747+LazySquirrel888@users.noreply.github.com>
2019-06-06 05:05:07 +0300

    Updates

---
+++ precache-manifest.87cd31207f82c45e5693a4ed12316416.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7456c3a834dfab617eecbfee75eebd1",
+    "url": "/react_barbershop/index.html"
+  },
+  {
+    "revision": "f46e8cbcc0afbd1027b4",
+    "url": "/react_barbershop/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "348367fc1cdd9ef49d43",
+    "url": "/react_barbershop/static/js/2.6b6d4a00.chunk.js"
+  },
+  {
+    "revision": "f46e8cbcc0afbd1027b4",
+    "url": "/react_barbershop/static/js/main.a8fcd86b.chunk.js"
+  },
+  {
+    "revision": "4fbeee9356c2e9e2e21a",
+    "url": "/react_barbershop/static/js/runtime~main.217ef99c.js"
+  }
+]);
