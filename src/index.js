205c68821c121e4ce4d55067961cf755168a8152
Tatiana_Dmitrieva <dmi3evatv@gmail.com>
2021-11-24 19:45:29 +0300

    add react-ga library

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 ReactDOM.render(
 <BrowserRouter>
 <App />
 </BrowserRouter>, document.getElementById('root'));
+ReactGA.initialize('UA-213600285-1');
 
 serviceWorker.unregister();
+
